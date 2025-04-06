7156fe8c8d5386fbd56dc0ddea8dfad6f9d15cc9
Bruno Ramos <162474087+brunoramossilva@users.noreply.github.com>
2025-04-06 17:45:05 -0300

    Update instruction_memory.v

--- Instruction Memory/instruction_memory_and_testbench.v
+++ Instruction Memory/instruction_memory_and_testbench.v
@@ -19,30 +19,3 @@ module InstructionMemory(
         instruction = memory[address >> 2]; // Divide por 4 para acessar corretamente
     end
 endmodule
-
-module InstructionMemory_Testbench;
-    reg [31:0] address;  // Endereço de entrada (PC)
-    wire [31:0] instruction; // Saída da instrução
-
-    // Instancia o módulo da Memória de Instruções
-    InstructionMemory uut (
-        .address(address),
-        .instruction(instruction)
-    );
-
-    initial begin
-        // Monitorando as mudanças na simulação
-        $monitor("Time: %0t | PC: %h | Instrução: %h", $time, address, instruction);
-
-        // Testa diferentes endereços
-        address = 32'd0;  #10; // Busca a instrução na posição 0
-        address = 32'd4;  #10; // Busca a instrução na posição 1
-        address = 32'd8;  #10; // Busca a instrução na posição 2
-        address = 32'd12; #10; // Busca a instrução na posição 3
-        address = 32'd16; #10; // Busca a instrução na posição 4
-
-        // Finaliza a simulação
-        #10;
-        $finish;
-    end
-endmodule
