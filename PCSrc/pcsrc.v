74ad340439541d7366c7d5c1ff87b3afaaa02c79
Bruno Ramos <162474087+brunoramossilva@users.noreply.github.com>
2025-04-12 15:17:50 -0300

    Create pcsrc.v

---
+++ PCSrc/pcsrc.v
@@ -0,0 +1,13 @@
+module PCSrc(
+    input wire Branch,         	// Sinal de branch
+    input wire Jump,           	// Sinal de jump
+  	input wire [31:0] PCPlus4,  // PCPlus4
+    input wire [31:0] PCBranch, // PC calculado para branch
+  	input wire [31:0] JumpAddr, // Endereço para jump
+    output wire [31:0] next_PC  // Próximo PC selecionado
+);
+
+assign next_PC = Jump ? JumpAddr: 
+                 (Branch ? PCBranch : PCPlus4);
+
+endmodule
