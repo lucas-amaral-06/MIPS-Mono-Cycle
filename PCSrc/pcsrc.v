5579b0a5d7ff4efd48883a5252591b769585b147
Bruno Ramos <162474087+brunoramossilva@users.noreply.github.com>
2025-04-13 17:37:04 -0300

    Update pcsrc.v

--- PCSrc/pcsrc.v
+++ PCSrc/pcsrc.v
@@ -1,13 +1,11 @@
 module PCSrc (
-    input wire Branch,         	// Sinal de branch
-    input wire Jump,           	// Sinal de jump
-  	input wire [31:0] PCPlus4,  // PCPlus4
-    input wire [31:0] PCBranch, // PC calculado para branch
-  	input wire [31:0] JumpAddr, // Endereço para jump
-    output wire [31:0] next_PC  // Próximo PC selecionado
+    input Branch,
+    input Jump,
+    input [31:0] PCPlus4,
+    input [31:0] PCBranch,
+    input [25:0] JumpImm,
+    output [31:0] next_PC
 );
-
-assign next_PC = Jump ? JumpAddr: 
-                 (Branch ? PCBranch : PCPlus4);
-
+    wire [31:0] JumpAddr = {PCPlus4[31:28], JumpImm, 2'b00};
+    assign next_PC = Jump ? JumpAddr : (Branch ? PCBranch : PCPlus4);
 endmodule
