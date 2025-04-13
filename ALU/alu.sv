a4d5171c75b5ce784da68b05fbf1966480dd2edd
Bruno Ramos <162474087+brunoramossilva@users.noreply.github.com>
2025-04-13 17:36:39 -0300

    Update alu.sv

--- ALU/alu.sv
+++ ALU/alu.sv
@@ -1,7 +1,7 @@
 module ALU (
     input [31:0] A,
     input [31:0] B,
-    input [3:0] op,          // Agora compatível com a ControlUnit (4 bits)
+    input [3:0] op,
     output reg [31:0] resultado,
     output zero_flag
 );
@@ -13,14 +13,9 @@ always @(*) begin
         4'b0010: resultado = A + B;    // ADD
         4'b0110: resultado = A - B;    // SUB
         4'b0111: resultado = (A < B) ? 32'd1 : 32'd0;  // SLT
-        4'b1010: begin                  // DIV (opcional)
-            if (B != 0) resultado = A / B;
-            else resultado = 32'hFFFFFFFF;
-        end
         default: resultado = 32'd0;
     endcase
 end
 
 assign zero_flag = (resultado == 0);
-
 endmodule
