11732f11baeabe1f1ba9c8c2a1b9036d13f5834e
Bruno Ramos <162474087+brunoramossilva@users.noreply.github.com>
2025-04-14 20:14:59 -0300

    Update register_file.sv

--- Register file/register_file.sv
+++ Register file/register_file.sv
@@ -1,28 +1,28 @@
 module register_file (
-    input clk,
-    input we,
-    input [4:0] A1,
-    input [4:0] A2,
-    input [4:0] A3,
-    input [31:0] WD3,
-    output [31:0] RD1,
-    output [31:0] RD2,
-    output [31:0] t0,  // $8
-    output [31:0] t1,  // $9
-    output [31:0] t2,  // $10
-    output [31:0] t3   // $11
+    input wire clk,
+    input wire we,
+    input wire [4:0] A1,
+    input wire [4:0] A2,
+    input wire [4:0] A3,
+    input wire [31:0] WD3,
+    output wire [31:0] RD1,
+    output wire [31:0] RD2,
+    output wire [31:0] t0,
+    output wire [31:0] t1,
+    output wire [31:0] t2,
+    output wire [31:0] t3,
+    output wire [31:0] s0
 );
-
     reg [31:0] regs [31:0];
     
     assign RD1 = (A1 == 5'd0) ? 32'd0 : regs[A1];
     assign RD2 = (A2 == 5'd0) ? 32'd0 : regs[A2];
     
-    // Conexões diretas para os registradores monitorados
     assign t0 = regs[8];
     assign t1 = regs[9];
     assign t2 = regs[10];
     assign t3 = regs[11];
+    assign s0 = regs[16];
     
     always @(posedge clk) begin
         if (we && A3 != 5'd0) begin
