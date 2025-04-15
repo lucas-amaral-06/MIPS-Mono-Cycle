98e1312077fc2e40138f934c4d9a79167e4b983e
Flavitche <fves@cin.ufpe.br>
2025-04-15 11:56:30 -0300

    Update pcplus4.sv

--- PcPlus4/pcplus4.sv
+++ PcPlus4/pcplus4.sv
@@ -1,25 +1,25 @@
-module PCPlus4(
+module PCPlus4(     // Módulo do registrador do PC
     input clk,
     input reset,
     input [31:0] next_PC,
     output reg [31:0] PC
 );
     always @(posedge clk or posedge reset) begin
-        if (reset)
+        if (reset)   // Zera o PC
             PC <= 32'b0;
-        else
+        else        // Atualiza o PC
             PC <= next_PC;
     end
 endmodule
 
-module PC_Adder(
+module PC_Adder(  // Incremento o valor atual do PC em 4
     input [31:0] PC,
     output [31:0] next_PC
 );
-    assign next_PC = PC + 32'd4;
+    assign next_PC = PC + 32'd4;  // Gera o netx_PC que será usado no módulo anterior
 endmodule
 
-module PC_System(
+module PC_System(  // Conecta os dois módulos anteriores
     input clk,
     input reset,
     output [31:0] PC
